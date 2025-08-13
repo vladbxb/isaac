d2b4af059ecca33a23e9fdd6ed4d6dff2643f960
aalex <alexandru.telejman@gmail.com>
2025-08-13 23:19:52 +0300

    Added tears + tears movement

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "raylib.h"
 
-enum Direction {
+enum Direction
+{
     UP,
     RIGHT,
     DOWN,
@@ -10,6 +11,7 @@ enum Direction {
 struct Tear
 {
     Vector2 tearPosition;
+    // Vector2 startPosition;
     enum Direction tearDirection;
 };
 
@@ -18,8 +20,8 @@ int main(void)
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    Color SKIN =  { 255, 215, 156, 255 };
-    Color BACKGROUND = { 84, 53, 30, 255 };
+    Color SKIN = {255, 215, 156, 255};
+    Color BACKGROUND = {84, 53, 30, 255};
 
     InitWindow(screenWidth, screenHeight, "isaac");
 
@@ -29,16 +31,22 @@ int main(void)
 
     float tearRadius = squareSize / 2;
 
-    struct Tear *spawnedTears = MemAlloc(sizeof(struct Tear)*1000);
+    struct Tear *spawnedTears = MemAlloc(sizeof(struct Tear) * 1000);
 
     unsigned int tearIndex = 0;
 
-    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+    double lastTearTime = 0;
+
+    double tearCooldown = 0.3;
+    float tearSpeed = 5;
+    // float range = 20;
+
+    SetTargetFPS(60);
 
     // Main game loop
-    while (!WindowShouldClose()) // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
-        // Update
+        // WASD movement
         if (IsKeyDown(KEY_D))
             squarePosition.x += 2.0f;
         if (IsKeyDown(KEY_A))
@@ -48,38 +56,79 @@ int main(void)
         if (IsKeyDown(KEY_S))
             squarePosition.y += 2.0f;
 
+        float tearOffset = 5;
 
-        float tearOffset = 5.0f;
-        if (IsKeyDown(KEY_RIGHT))
+        Vector2 squareCenter = {squarePosition.x + squareDimensions.x / 2, squarePosition.y + squareDimensions.y / 2};
+
+        double currentTime = GetTime();
+
+        // Tear spawning
+        if (currentTime - lastTearTime >= tearCooldown)
         {
-            Vector2 tearPosition = { squarePosition.x + tearOffset, squarePosition.y };
-            (spawnedTears + tearIndex * sizeof(struct Tear))->tearPosition = tearPosition;
-            (spawnedTears + tearIndex * sizeof(struct Tear))->tearDirection = RIGHT;
-            ++tearIndex;
+            if (IsKeyDown(KEY_RIGHT))
+            {
+                spawnedTears[tearIndex].tearPosition = (Vector2){squareCenter.x + tearOffset, squareCenter.y};
+
+                spawnedTears[tearIndex].tearDirection = RIGHT;
+                ++tearIndex;
+                lastTearTime = currentTime;
+            }
+            else if (IsKeyDown(KEY_LEFT))
+            {
+                spawnedTears[tearIndex].tearPosition = (Vector2){squareCenter.x - tearOffset, squareCenter.y};
+                spawnedTears[tearIndex].tearDirection = LEFT;
+                ++tearIndex;
+                lastTearTime = currentTime;
+            }
+            else if (IsKeyDown(KEY_UP))
+            {
+                spawnedTears[tearIndex].tearPosition = (Vector2){squareCenter.x, squareCenter.y - tearOffset};
+                spawnedTears[tearIndex].tearDirection = UP;
+                ++tearIndex;
+                lastTearTime = currentTime;
+            }
+            else if (IsKeyDown(KEY_DOWN))
+            {
+                spawnedTears[tearIndex].tearPosition = (Vector2){squareCenter.x, squareCenter.y + tearOffset};
+                spawnedTears[tearIndex].tearDirection = DOWN;
+                ++tearIndex;
+                lastTearTime = currentTime;
+            }
         }
-            // spawn tear to the right
-        if (IsKeyDown(KEY_LEFT))
-            // spawn tear to the left
-        if (IsKeyDown(KEY_UP))
-            // spawn tear at the top
-        if (IsKeyDown(KEY_DOWN))
-            // spawn tear at the bottom
+
+        // Update tears positions
+        for (unsigned int i = 0; i < tearIndex; ++i)
+        {
+            switch (spawnedTears[i].tearDirection)
+            {
+            case RIGHT:
+                spawnedTears[i].tearPosition.x += tearSpeed;
+                break;
+            case LEFT:
+                spawnedTears[i].tearPosition.x -= tearSpeed;
+                break;
+            case UP:
+                spawnedTears[i].tearPosition.y -= tearSpeed;
+                break;
+            case DOWN:
+                spawnedTears[i].tearPosition.y += tearSpeed;
+                break;
+            }
+        }
 
         // Draw
         BeginDrawing();
-
         ClearBackground(BACKGROUND);
 
-        DrawText("move the square with wasd", 10, 10, 20, RAYWHITE);
-
+        DrawText("Move the square with WASD", 10, 10, 20, RAYWHITE);
         DrawRectangleV(squarePosition, squareDimensions, SKIN);
 
         for (unsigned int i = 0; i < tearIndex; ++i)
-            DrawCircleV((spawnedTears + i * sizeof(struct Tear))->tearPosition, tearRadius, BLUE);
+            DrawCircleV(spawnedTears[i].tearPosition, tearRadius, BLUE);
 
         EndDrawing();
     }
 
-    CloseWindow(); // Close window and OpenGL context
+    CloseWindow();
     return 0;
 }
